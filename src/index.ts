f31ce734378c8afe41420fef0f335980a80aa807
LucasZanini096 <lucas608@outlook.com>
2025-01-08 15:39:00 -0300

    feat: index.ts

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/*
+
+Vantagens e desvantagens do S.O.L.I.D.
+
+Vantagens:
+
+- Código modular
+- Código reutilizável (D.R.Y - Don't Repeat Yourself)
+- Código testável, baixo acoplamento, pois as classes depende de abstrações e não de classes concetas
+- Baixo acoplamento e alta coesão/coerência
+- Código expansível
+- Separation of Concerns (SoC)
+- Fácil manutenção
+
+Desvantagens:
+- Complexidade
+- Quantidade de código digitado aumenta
+- Tenha cuidado com: YAGNI (You Aren't Gonna Need It) e KISS (Keep It Simple, Stupid!)
+
+*/
